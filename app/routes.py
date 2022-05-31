59c4ad9ab0abbd6c89af34219df421ab53db0b2c
s-301-06 <s-301-06@gumiforest.uek.krakow.pl>
2022-05-31 14:41:58 +0200

    31/05/2022 KH

---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+@app.route('/index/<name>')
+def index(name="Hello World"):
+    return render_template("index.html.jinja", text=name)
+
+@app.route('/product/<product_id>')
+def product(product_id):

7c8e339d95e9d183e65bcab8bafc6ba45fcc5019
s-007-12 <s-007-12@gumiforest.ae.krakow.pl>
2022-06-17 09:03:54 +0200

    17/06/2022 KH

--- app/routes.py
+++ app/routes.py
@@ -1,36 +1,8 @@
 from app import app
 from flask import render_template, redirect, url_for, request
-import requests
-import json
-from bs4 import BeautifulSoup
 import os
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
 from app.models.product import Product
 
-def get_item(ancestor, selector, attribute=None, return_list=False):
-    try:
-        if return_list:
-            return [item.get_text().strip() for item in ancestor.select(selector)]
-        if attribute:
-            return ancestor.select_one(selector)[attribute]
-        return ancestor.select_one(selector).get_text().strip()
-    except (AttributeError, TypeError):
-        return None
-
-selectors = {
-    "author": ["span.user-post__author-name"],
-    "recommendation": ["span.user-post__author-recomendation > em"],
-    "stars": ["span.user-post__score-count"],
-    "content": ["div.user-post__text"],
-    "useful": ["button.vote-yes > span"],
-    "useless": ["button.vote-no > span"],
-    "published": ["span.user-post__published > time:nth-child(1)", "datetime"],
-    "purchased": ["span.user-post__published > time:nth-child(2)", "datetime"],
-    "pros": ["div[class$=positives] ~ div.review-feature__item", None, True],
-    "cons": ["div[class$=negatives] ~ div.review-feature__item", None, True]
-}
 
 @app.route('/')
 def index():
@@ -41,9 +13,9 @@ def extract():
     if request.method == "POST":
         product_id = request.form.get("product_id")
         product = Product(product_id)
-        product.extract_product()
-        
-        
+        product.extract_product().process_stats().draw_charts()
+        # product.save_stats()
+        product.save_opinions()
         return redirect(url_for("product", product_id=product_id))
     else:
         return render_template("extract.html.jinja")
@@ -59,27 +31,8 @@ def author():
 
 @app.route('/product/<product_id>')
 def product(product_id):
-    opinions = pd.read_json(f"app/opinions/{product_id}.json")
-    opinions.stars = opinions.stars.map(lambda x: float(x.split("/")[0].replace(",", ".")))
-    
-    recommendation = opinions.recommendation.value_counts(dropna = False).sort_index().reindex(["Nie polecam", "Polecam", None])
-    recommendation.plot.pie(
-        label="", 
-        autopct="%1.1f%%", 
-        colors=["crimson", "forestgreen", "lightskyblue"],
-        labels=["Nie polecam", "Polecam", "Nie mam zdania"]
-    )
-    plt.title("Rekomendacja")
-    plt.savefig(f"app/static/plots/{product_id}_recommendations.png")
-    plt.close()
-
-    stars = opinions.stars.value_counts().sort_index().reindex(list(np.arange(0,5.5,0.5)), fill_value=0)
-    stars.plot.bar()
-    plt.title("Oceny produktu")
-    plt.xlabel("Liczba gwiazdek")
-    plt.ylabel("Liczba opinii")
-    plt.grid(True)
-    plt.xticks(rotation=0)
-    plt.savefig(f"app/static/plots/{product_id}_stars.png")
-    plt.close()
+    product = Product(product_id)
+    product.read_from_json()
+    opinions = product.opinions_do_df()
+    stats = product.stats_to_dict()
     return render_template("product.html.jinja", stats=stats, product_id=product_id, opinions=opinions)
